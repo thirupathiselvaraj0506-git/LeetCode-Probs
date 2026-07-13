e3b2ec6b50a4f331bb27f137d23b501503338801
Thirupathi Selvaraj V R <thirupathiselvaraj0506@gmail.com>
2026-07-13 08:33:01 +0530

    Time: 0 ms (100%), Space: 8.4 MB (48.96%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int>ans;
+        for(int i=1;i<=9;i++){
+            int temp=i;
+            for(int j=i+1;j<=9 && temp<=high;j++){
+                temp=temp*10+j;
+                if(temp>=low && temp<=high) ans.push_back(temp);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
