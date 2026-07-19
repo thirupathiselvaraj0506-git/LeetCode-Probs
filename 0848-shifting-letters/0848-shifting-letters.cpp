8c780e1b5f72bcd8d0cfec20a93e358e0123a99a
Thirupathi Selvaraj V R <thirupathiselvaraj0506@gmail.com>
2026-07-19 06:28:34 +0530

    Time: 1128 ms (5.4%), Space: 71.8 MB (47.97%) - LeetHub

---
+++ 0848-shifting-letters/0848-shifting-letters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        // int N=shifts.size();
+        // vector<int>suff(N);
+        // suff[N-1]=shifts[N-1];
+        // for(int i=N-2;i>=0;i--){
+        //     suff[i]=suff[i+1]+shifts[i];
+        // }
+        int sum=0;
+        for(int i=s.size()-1;i>=0;i--){
+            sum=(sum+shifts[i])%26;
+            s[i]=((s[i]-'a'+sum)%26) +'a';
+            cout<<s[i]<<" ";
+        }
+        return s;
+    }
+};
