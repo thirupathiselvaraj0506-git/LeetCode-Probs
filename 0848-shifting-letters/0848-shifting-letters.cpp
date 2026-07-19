589788416a07fef030bea753a770f65c18441e7d
Thirupathi Selvaraj V R <thirupathiselvaraj0506@gmail.com>
2026-07-19 06:30:39 +0530

    Time: 4 ms (59.25%), Space: 71.7 MB (67.83%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.cpp
+++ 0848-shifting-letters/0848-shifting-letters.cpp
@@ -7,11 +7,11 @@ public:
         // for(int i=N-2;i>=0;i--){
         //     suff[i]=suff[i+1]+shifts[i];
         // }
-        int sum=0;
-        for(int i=s.size()-1;i>=0;i--){
+        long long sum=0;
+        for(int i=shifts.size()-1;i>=0;i--){
             sum=(sum+shifts[i])%26;
             s[i]=((s[i]-'a'+sum)%26) +'a';
-            cout<<s[i]<<" ";
+            // cout<<s[i]<<" ";
         }
         return s;
     }
