6a1ca7b26e853b6a09c8315500fbf4fc3d43d903
Thirupathi Selvaraj V R <thirupathiselvaraj0506@gmail.com>
2026-07-19 13:51:48 +0530

    Time: 2 ms (25.96%), Space: 10.1 MB (7.86%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        stack<char>st;
+        unordered_set<char>myset;
+        unordered_map<char,int>lastocc;
+        for(int i=0;i<s.size();i++){
+            lastocc[s[i]]=i;
+        }
+        for(int i=0;i<s.size();i++){
+            if(!myset.count(s[i])){
+                while(!st.empty() && s[i]<st.top() && i<lastocc[st.top()]){
+                    myset.erase(st.top());
+                    st.pop();
+                }
+                myset.insert(s[i]);
+                st.push(s[i]);
+            }
+        }
+        string ans="";
+        while(!st.empty()){
+            ans=st.top()+ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
